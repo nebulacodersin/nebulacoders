2f9edd30b3387716fcdbc7e42da6db07b1b2b9fb
Nebula Coders <167498992+nebulacodersin@users.noreply.github.com>
2024-06-04 19:56:14 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -20,24 +20,31 @@ form.addEventListener("submit",(e)=>{
 
 function authentication(username,password){
     if(username === "22105128040" && password === "25092004"){
+        localStorage.setItem("username","Nishant");
         return true
     }
     else if(username === "22105128050" && password === "26082004"){
+        localStorage.setItem("username","Suhani");
         return true
     }
     else if(username === "22105128022" && password === "24102003"){
+        localStorage.setItem("username","Shubham");
         return true
     }
     else if(username === "22105128035" && password === "11092004"){
+        localStorage.setItem("username","Saurav");
         return true
     }
     else if(username === "23105128903" && password === "30052003"){
+        localStorage.setItem("username","Kriti");
         return true
     }
     else if(username === "yasmin" && password === "samina"){
+        localStorage.setItem("username","Samina");
         return true
     }
     else if(username === "22110128013" && password === "jiyaraj"){
+        localStorage.setItem("username","Jiya");
         return true
     }
     else{
