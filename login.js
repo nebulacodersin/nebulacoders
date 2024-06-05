fb0a9580f0e0705e65833cc0f4fcad2b1a14b2c2
Nebula Coders <167498992+nebulacodersin@users.noreply.github.com>
2024-06-06 03:20:47 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -51,26 +51,32 @@ form.addEventListener("submit",(e)=>{
 function authentication(username,password){
     if(username === "22105128040" && password === "25092004"){
         localStorage.setItem("username","Nishant");
+        localStorage.setItem("imgurl","nishant.jpg")
         return true
     }
     else if(username === "22105128050" && password === "26082004"){
         localStorage.setItem("username","Suhani");
+        localStorage.setItem("imgurl","suhani.jpg")
         return true
     }
     else if(username === "22105128022" && password === "24102003"){
         localStorage.setItem("username","Shubham");
+        localStorage.setItem("imgurl","shubham.jpg")
         return true
     }
     else if(username === "22105128035" && password === "11092004"){
         localStorage.setItem("username","Saurav");
+        localStorage.setItem("imgurl","saurav.jpg")
         return true
     }
     else if(username === "23105128903" && password === "30052003"){
         localStorage.setItem("username","Kriti");
+        localStorage.setItem("imgurl","kriti.jpg")
         return true
     }
     else if(username === "yasmin" && password === "samina"){
         localStorage.setItem("username","Samina");
+        localStorage.setItem("imgurl","samina.webp")
         return true
     }
     else if(username === "22110128013" && password === "jiyaraj"){
